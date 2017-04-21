aba589e0e7b7c3b229ddae889a323f0f2dd75f18
Axel Hodler <derxor@gmail.com>
2017-04-21 10:18:26 +0200

    can handle response fails in test

--- src/server/blockchaininfo-gateway_test.js
+++ src/server/blockchaininfo-gateway_test.js
@@ -2,12 +2,22 @@ const test = require('tape');
 const rp = require('request-promise-native')
 const server = require('./server')
 
-test('can query latestblock', (assert) => {
+test('can query latestblock', assert => {
   const runningServer = server.start()
   rp('http://localhost:3001/latestblock').then(resp => {
     assert.equal(JSON.parse(resp).latest_block, 453470)
   }).then(() => {
     runningServer.close()
+    assert.end()
+  })
+})
+
+test('handles server not present gracefully', assert => {
+  rp('http://localhost:3001/latestblock').then(() => {
+    assert.fail('server should not respond')
+    assert.end()
+  }).catch(() => {
+    assert.true('error was thrown')
     assert.end()
   })
 })
