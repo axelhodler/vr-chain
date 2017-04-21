0bc4bb3c14503aad22ea83537ccfe6b47e1f1018
Axel Hodler <derxor@gmail.com>
2017-04-21 15:29:19 +0200

    clean up after test

---
+++ scripts/test.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+./node_modules/browserify/bin/cmd.js src/*_test.js | ./node_modules/tape-run/bin/run.js
+
+# throw away whatever browserify created above (some .source.<timestamp>.html file)
+ls -ABt1 | head -n3 | grep .source.*.html | xargs rm
