3249eb2dbfbbf8f2f62bfc8a2218f92b83b09806
Axel Hodler <derxor@gmail.com>
2017-04-21 12:48:26 +0200

    the slime!

--- src/server/server.js
+++ src/server/server.js
@@ -1,12 +1,18 @@
 var express = require('express')
 var cors = require('cors')
 var app = express()
-app.use(cors());
+app.use(cors())
+
+app.get('/blocks/453471', (req, res) => {
+  res.send({
+    'id': 453471
+  })
+})
 
 app.get('/latestblock', (req, res) => {
   res.send({
     'latest_block': 453470
-  });
+  })
 })
 
 module.exports = {
