d0312d42e3c87b7690765cf1a131e6fa35cf519e
Axel Hodler <derxor@gmail.com>
2017-04-28 17:30:45 +0200

    colorful test output

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-./node_modules/browserify/bin/cmd.js src/*_test.js -t [ babelify --presets [ es2015 ] ] | ./node_modules/tape-run/bin/run.js
+./node_modules/browserify/bin/cmd.js src/*_test.js -t [ babelify --presets [ es2015 ] ] \
+  | ./node_modules/tape-run/bin/run.js \
+  | node_modules/.bin/tap-spec
 
 # throw away whatever browserify created above (some .source.<timestamp>.html file)
 ls -ABt1 | head -n3 | grep .source.*.html | xargs rm
