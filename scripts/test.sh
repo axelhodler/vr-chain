f2f108596d70c474f598f35b6447c12cee8f91fe
Axel Hodler <derxor@gmail.com>
2017-04-28 15:14:52 +0200

    add browserify es2015 support

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-./node_modules/browserify/bin/cmd.js src/*_test.js | ./node_modules/tape-run/bin/run.js
+./node_modules/browserify/bin/cmd.js src/*_test.js -t [ babelify --presets [ es2015 ] ] | ./node_modules/tape-run/bin/run.js
 
 # throw away whatever browserify created above (some .source.<timestamp>.html file)
 ls -ABt1 | head -n3 | grep .source.*.html | xargs rm
